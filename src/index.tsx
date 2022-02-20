fb6084d837149a88b9ad844e63df62ba985c580e
SteadyJingo <jacob.w.corum@gmail.com>
2022-02-19 20:56:09 -0700

    satisfying eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as Sentry from "@sentry/react"
+import { BrowserTracing } from "@sentry/tracing"
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 Sentry.init({
   dsn: "https://5b42234cc9e74a6e82b642cdef83daf4@o1140961.ingest.sentry.io/6219856",
@@ -27,4 +27,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()

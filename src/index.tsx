2becfc7e45bf6deba6c880f66ac4c8d574702952
SteadyJingo <jacob.w.corum@gmail.com>
2022-02-19 20:37:56 -0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://5b42234cc9e74a6e82b642cdef83daf4@o1140961.ingest.sentry.io/6219856",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV
+});
 
 ReactDOM.render(
   <React.StrictMode>
